5416ed08db318d6cb791a0f71afa32e0c8553a79
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-09-30 22:05:38 +0800

    added cart delete api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
@@ -51,6 +51,12 @@ async function run() {
             const query = {email: email}
             const result = await cartcollection.find(query).toArray();
             res.send(result);
+        })
+        app.delete('/cart/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartcollection.deleteOne(query);
+            res.send(result)
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
